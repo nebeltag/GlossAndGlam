d4b8dc1c9336859446f74c78ff243453a420406d
nebeltag <nebeltag@mail.ru>
2024-05-14 00:47:54 +0300

    разработка выпадающего меню в хедер

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,12 @@
 import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
+
+const servicesMenuLink = document.querySelectorAll('.services-menu');
+const servicesMenuSublist = document.querySelectorAll('.services-menu__list');
+console.log(servicesMenuLink[0]);
+console.log(servicesMenuSublist[0]);
+
+servicesMenuLink[0].addEventListener("click", function () {
+  servicesMenuSublist[0].classList.toggle("_show");
+})
