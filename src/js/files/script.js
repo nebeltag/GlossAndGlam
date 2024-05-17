9aead8dbabe321d35d8fd336c7912dd101f0d977
nebeltag <nebeltag@mail.ru>
2024-05-17 06:59:49 +0300

    Продолжение работы над хедером

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -3,11 +3,38 @@ import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
 
-const servicesMenuLink = document.querySelectorAll('.services-menu');
-const servicesMenuSublist = document.querySelectorAll('.services-menu__list');
-console.log(servicesMenuLink[0]);
-console.log(servicesMenuSublist[0]);
+//======Вкл/Выкл субменю Services в header-nav=================
 
-servicesMenuLink[0].addEventListener("click", function () {
-  servicesMenuSublist[0].classList.toggle("_show");
-})
+const servicesMenuLink = document.querySelectorAll('.services-menu')[0];
+const servicesMenuSublist = document.querySelectorAll('.services-menu__list')[0];
+
+const servicesMenuItems = [servicesMenuLink, servicesMenuSublist];
+
+const showServicesMenu = (e) => {
+  (e.target == servicesMenuLink) ?
+    (servicesMenuItems.forEach(item => {
+      item.classList.toggle('_show-services')
+    })) :
+    (servicesMenuItems.forEach(item => {
+      item.classList.remove('_show-services')
+    }))
+}
+
+document.addEventListener("click", showServicesMenu);
+
+//==============================================================
+
+//======Вкл/Выкл search-form в header-action=================
+
+const actionSearch = document.querySelectorAll('.action-search')[0];
+const actionSearchForm = document.querySelectorAll('.header-search__form')[0];
+const actionSearchInput = document.getElementById('action-search-input');
+
+const showActionSearch = (e) => {
+  (e.target == actionSearch) ?
+    actionSearchForm.classList.toggle('_show-action-search-form')
+    : (e.target == actionSearchInput) ? false
+      : actionSearchForm.classList.remove('_show-action-search-form')
+}
+
+document.addEventListener("click", showActionSearch);
