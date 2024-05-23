a8639ff457505be1a5862aebb6fa6116cc61bda1
nebeltag <nebeltag@mail.ru>
2024-05-23 19:23:08 +0300

    разработка мобильного меню

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -38,3 +38,20 @@ const showActionSearch = (e) => {
 }
 
 document.addEventListener("click", showActionSearch);
+
+
+const buttons = document.querySelectorAll(".icon-menu");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const currentState = button.getAttribute("data-state");
+
+    if (!currentState || currentState === "closed") {
+      button.setAttribute("data-state", "opened");
+      button.setAttribute("aria-expanded", "true");
+    } else {
+      button.setAttribute("data-state", "closed");
+      button.setAttribute("aria-expanded", "false");
+    }
+  });
+});
